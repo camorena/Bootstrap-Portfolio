64531bb6822396bb151a20551b7e3536cdd72545
camorena <camoren@gmail.com>
2019-04-06 09:34:10 -0500

    add link

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+document.getElementById("liri").onclick = function() {
+  myFunction();
+};
+
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function myFunction() {
+  window.open("https://github.com/camorena/liri-node-app.git", "_blank");
+}
